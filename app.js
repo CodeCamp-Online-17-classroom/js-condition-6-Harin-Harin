43eef7280eea208c02f1c092d67c62cb4dc64473
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-03 02:14:19 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// write code here
+let a = prompt("พิมตัวเลข")
+if (isNaN(a)){
+    alert('Invalid number');
+} else if(a%2 == 1 ) {
+    alert('Odd number');}
+    else if (a%2 == 0 ) {
+            alert('Even number');}
